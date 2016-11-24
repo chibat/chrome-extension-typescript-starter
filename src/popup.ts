bb7acd660bf034710d47a2cc45b9e365ec9d7032
t1000leaf <tomofumi.chiba@gmail.com>
2016-11-24 20:39:51 +0900

    -

--- src/popup.ts
+++ src/popup.ts
@@ -1,17 +1,13 @@
 import * as moment from 'moment';
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
+  document.addEventListener('DOMContentLoaded', function() {
+    const queryInfo = {
+      active: true,
+      currentWindow: true
+    };
 
-document.addEventListener('DOMContentLoaded', function() {
-  const queryInfo = {
-    active: true,
-    currentWindow: true
-  };
-
-  chrome.tabs.query(queryInfo, function(tabs) {
-    document.getElementById('url').textContent = tabs[0].url;
-    document.getElementById('time').textContent = moment().format('YYYY-MM-DD HH:mm:ss');
+    chrome.tabs.query(queryInfo, function(tabs) {
+      document.getElementById('url').textContent = tabs[0].url;
+      document.getElementById('time').textContent = moment().format('YYYY-MM-DD HH:mm:ss');
+    });
   });
-});
