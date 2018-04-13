a2f1e0eda5c3661de476d4c477e49ae655cf49e0
chibat <tomofumi.chiba@gmail.com>
2018-04-13 21:37:17 +0900

    -

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -4,14 +4,5 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    devtool: 'source-map',
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        })
-    ],
-    mode: 'production',
+    mode: 'production'
 });
