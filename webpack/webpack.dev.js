9437865d9bc183b222d602536ab6306532eccf7c
chibat <tomofumi.chiba@gmail.com>
2019-05-03 01:14:10 +0900

    remove unused variable

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,4 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
