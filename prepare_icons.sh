aa935bc2037c76e0cde6c451e35439473e4b0a4c
Lars Melchior <TheLartians@users.noreply.github.com>
2021-01-02 14:51:35 +0100

    remove name and afdesign icon

--- prepare_icons.sh
+++ prepare_icons.sh
@@ -1,6 +1,6 @@
 
 if [ ! -f public/icon128.png ]; then
-  ICON=$(pwd)/resources/export/icon.svg
+  ICON=$(pwd)/resources/icon.svg
   echo "generating icons from ${ICON}"
   convert -density 128x128 -background none -resize 128x128 ${ICON} public/icon128.png
   convert -density 48x48 -background none -resize 48x48 ${ICON} public/icon48.png
