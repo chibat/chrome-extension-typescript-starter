b544390bb931d8de2006de8b63f6ad6ad106a5d5
Anton Ödman <anton.odman@gmail.com>
2021-01-16 17:44:53 +0100

    Fixes #26 - auto import all files for webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,43 +1,44 @@
 const webpack = require("webpack");
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const srcDir = '../src/';
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = "../src/";
+var glob = require("glob");
 
 module.exports = {
-    entry: {
-        popup: path.join(__dirname, srcDir + 'popup.ts'),
-        options: path.join(__dirname, srcDir + 'options.ts'),
-        background: path.join(__dirname, srcDir + 'background.ts'),
-        content_script: path.join(__dirname, srcDir + 'content_script.ts')
-    },
+    entry: glob
+        .sync(path.join(__dirname, srcDir + "*.ts"))
+        .reduce(function (obj, el) {
+            obj[path.parse(el).name] = el;
+            return obj;
+        }, {}),
     output: {
-        path: path.join(__dirname, '../dist/js'),
-        filename: '[name].js'
+        path: path.join(__dirname, "../dist/js"),
+        filename: "[name].js",
     },
     optimization: {
         splitChunks: {
-            name: 'vendor',
-            chunks: "initial"
-        }
+            name: "vendor",
+            chunks: "initial",
+        },
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js']
+        extensions: [".ts", ".tsx", ".js"],
     },
     plugins: [
         // exclude locale files in moment
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         new CopyPlugin({
-            patterns: [{ from: '.', to: '../', context: 'public' }],
-            options: {}
+            patterns: [{ from: ".", to: "../", context: "public" }],
+            options: {},
         }),
-    ]
+    ],
 };
