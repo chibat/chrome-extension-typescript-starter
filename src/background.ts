68b1b8f89ce216ebc66e619de596c89a32fb9406
chibat <tomofumi.chiba@gmail.com>
2021-01-17 23:03:00 +0900

    add prettier

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,6 @@
-
 function polling() {
-    console.log('polling');
-    setTimeout(polling, 1000 * 30);
+  console.log("polling");
+  setTimeout(polling, 1000 * 30);
 }
 
 polling();
-
--- src/sum.ts
+++ src/sum.ts
@@ -1,4 +1,3 @@
 export function sum(x: number, y: number) {
-    return x + y;
+  return x + y;
 }
-
