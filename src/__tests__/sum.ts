e10c0434d97fd67ab5a3fc0aeb681f26e527cb86
renge_houdo <tinamini.7865@gmail.com>
2022-07-24 01:27:52 +0900

    追加で不要なものを削除

--- src/__tests__/sum.ts
+++
@@ -1,9 +0,0 @@
-import { sum } from "../sum";
-
-test("1 + 1 = 2", () => {
-  expect(sum(1, 1)).toBe(2);
-});
-
-test("1 + 2 != 2", () => {
-  expect(sum(1, 2)).not.toBe(2);
-});
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,8 +5,6 @@ const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
     entry: {
-      popup: path.join(srcDir, 'popup.tsx'),
-      options: path.join(srcDir, 'options.tsx'),
       background: path.join(srcDir, 'background.ts'),
       content_script: path.join(srcDir, 'content_script.tsx'),
     },
