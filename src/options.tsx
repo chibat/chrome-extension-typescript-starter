cd750672036dbe2eb1a130a12d6e0042723dfe13
InbarDanieli <inbar0308@gmail.com>
2023-03-25 18:48:46 +0300

    feat: update react to version 18

--- src/options.tsx
+++ src/options.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const Options = () => {
   const [color, setColor] = useState<string>("");
@@ -68,9 +68,10 @@ const Options = () => {
   );
 };
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
     <Options />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
