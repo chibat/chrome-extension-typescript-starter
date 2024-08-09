bddfdf0a0490605699c62b6d3c6029ab793c67d0
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-08-09 12:05:37 +0200

    Migrate to octokit

--- src/content/handlePrs.ts
+++ src/content/handlePrs.ts
@@ -1,41 +1,47 @@
+import { Octokit } from "@octokit/rest";
 import { Config } from "../services";
 
-export function handlePrs(config: Config, pullsApiUrl: string) {
-  fetch(pullsApiUrl, {
-    headers: {
-      Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${config.pat}`,
-      "X-GitHub-Api-Version": "2022-11-28",
+export async function handlePrs(config: Config) {
+  const octokit = new Octokit({
+    auth: config.pat,
+    baseUrl: config.ghBaseUrl,
+    log: {
+      debug: console.log,
+      info: console.log,
+      warn: console.warn,
+      error: console.error,
     },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // @ts-ignore
-      const mapped = data.map((d) => ({
-        prNumber: d.number,
-        headRef: d.head.ref,
-        baseRef: d.base.ref,
-      }));
-
-      const prRows = document.querySelectorAll("div[id^=issue_]");
-      prRows.forEach((prRow) => {
-        const prNumber = prRow.id.split("_")[1];
-        const prData = mapped.find(
-          // @ts-ignore
-          (d) => d.prNumber === parseInt(prNumber)
-        );
-        if (prData) {
-          const text = `${prData.baseRef} <-- ${prData.headRef}`;
-          const spanEl = document.createElement("span");
-          spanEl.textContent = text;
-          spanEl.classList.add("IssueLabel");
-          spanEl.classList.add("hx_IssueLabel");
-          prRow.children[0].children[2].appendChild(spanEl);
-        }
-      });
-    })
-    .catch((e) => {
-      alert("Error fetching PR data. Check console");
-      console.error(e);
+  });
+  try {
+    const { data } = await octokit.pulls.list({
+      owner: config.org,
+      repo: config.repo,
+      state: "open",
+      per_page: 100,
+      page: 1,
     });
+
+    const prs = data.map((pr) => ({
+      prNumber: pr.number,
+      headRef: pr.head.ref,
+      baseRef: pr.base.ref,
+    }));
+
+    const prRows = document.querySelectorAll("div[id^=issue_]");
+    prRows.forEach((prRow) => {
+      const prNumber = prRow.id.split("_")[1];
+      const prData = prs.find((pr) => pr.prNumber === parseInt(prNumber));
+      if (prData) {
+        const text = `${prData.baseRef} <-- ${prData.headRef}`;
+        const spanEl = document.createElement("span");
+        spanEl.textContent = text;
+        spanEl.classList.add("IssueLabel");
+        spanEl.classList.add("hx_IssueLabel");
+        prRow.children[0].children[2].appendChild(spanEl);
+      }
+    });
+  } catch (err) {
+    alert("Error fetching PR data. Check console");
+    console.error(err);
+  }
 }
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -7,14 +7,13 @@ getConfig({
 });
 
 function handleContent(config: Config) {
-  const pullsApiUrl = `${config.ghBaseUrl}/repos/${config.org}/${config.repo}/pulls`;
   const baseUiUrl = `${config.ghBaseUrl.replace("/api/v3", "")}/${config.org}/${
     config.repo
   }`;
   const prsUiUrl = `${baseUiUrl}/pulls`;
   const prUiUrl = `${baseUiUrl}/pull`;
 
-  if (window.location.href.startsWith(prsUiUrl)) handlePrs(config, pullsApiUrl);
+  if (window.location.href.startsWith(prsUiUrl)) handlePrs(config);
   if (window.location.href.startsWith(prUiUrl)) handlePr();
 }
 
