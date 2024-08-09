4f2fe76db4193177df9477e0751940f19056fbdd
HANS KREBS <hans.krebs@mercedes-benz.com>
2024-08-09 12:30:31 +0200

    Show loading spinner

--- src/content/handlePrs.ts
+++ src/content/handlePrs.ts
@@ -5,14 +5,9 @@ export async function handlePrs(config: Config) {
   const octokit = new Octokit({
     auth: config.pat,
     baseUrl: config.ghBaseUrl,
-    log: {
-      debug: console.log,
-      info: console.log,
-      warn: console.warn,
-      error: console.error,
-    },
   });
   try {
+    showSpinner();
     const { data } = await octokit.pulls.list({
       owner: config.org,
       repo: config.repo,
@@ -43,5 +38,66 @@ export async function handlePrs(config: Config) {
   } catch (err) {
     alert("Error fetching PR data. Check console");
     console.error(err);
+  } finally {
+    hideSpinner();
   }
+}
+
+function showSpinner() {
+  let loadingOverlay = document.querySelector(".ghuibooster__overlay");
+  if (loadingOverlay) {
+    loadingOverlay.classList.remove("ghuibooster__hidden");
+    return;
+  }
+
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = `
+    .ghuibooster__overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 9999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+      
+    .ghuibooster__overlay.ghuibooster__hidden {
+        display: none;
+    }
+
+    .ghuibooster__spinner {
+      border: 12px solid #f3f3f3;
+      border-top: 12px solid #0d1318;
+      border-radius: 50%;
+      width: 80px;
+      height: 80px;
+      animation: ghuibooster__spin 2s linear infinite;
+    }
+
+    @keyframes ghuibooster__spin {
+      0% { transform: rotate(0deg); }
+      100% { transform: rotate(360deg); }
+    }
+  `;
+  document.getElementsByTagName("head")[0].appendChild(style);
+
+  loadingOverlay = document.createElement("div");
+  loadingOverlay.classList.add("ghuibooster__overlay");
+
+  const loadingSpinner = document.createElement("div");
+  loadingSpinner.classList.add("ghuibooster__spinner");
+
+  loadingOverlay.appendChild(loadingSpinner);
+  const body = document.querySelector("body");
+  body?.appendChild(loadingOverlay);
+}
+
+function hideSpinner() {
+  const loadingOverlay = document.querySelector(".ghuibooster__overlay");
+  loadingOverlay?.classList.add("ghuibooster__hidden");
 }
